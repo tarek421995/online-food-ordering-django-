38626d698dc3d78e430983ef325a2f12fad69112
tarek421995 <tarek421995@gmail.com>
2020-11-29 23:45:45 +0200

    ready for heroku

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = '*qwvu5+co-z4(@my1c(90h*)cbejnp)%d+k1f645sh(9ul=&07'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", ".herokuapp.com"]
 
 
-AUTH_USER_MODEL= 'webapp.User'
+AUTH_USER_MODEL = 'webapp.User'
 # Application definition
 
 INSTALLED_APPS = [
